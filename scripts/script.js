79b46341cebfee66c22ae335b692b4739869b94d
FES001 <firdavs7794948@gmail.com>
2025-03-10 21:34:09 +0500

    Working on it.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const playerChoiceButtons = document.querySelectorAll('.main-middle-picking-button');
+const pickingContainer = document.querySelector('.main-middle-picking');
+const outcomeContainer = document.querySelector('.main-middle-outcome');
 
 // GAME
 const game = {
@@ -46,4 +48,39 @@ const game = {
             }
         }
     },
+    choiceNames: ['rock', 'paper', 'scissors', 'lizard', 'spock'],
+};
+
+// PLAYER PICKING
+
+for (let i = 0; i < playerChoiceButtons.length; i++) {
+    playerChoiceButtons[i].addEventListener('click', () => {
+        // SAVING THE PICKED BY THE PLAYER
+        game.playerPicked = playerChoiceButtons[i].value;
+
+        // SAVING THE PICKED BY THE HOUSE
+        theHousePicking();
+    }); 
+};
+
+// THE HOUSE PICKING
+
+function theHousePicking() {
+    const pickedByHouse = game.choiceNames[Math.floor(Math.random() * game.choiceNames.length)];
+    game.housePicked = pickedByHouse;
+
+    // CHECK WHO WON THE GAME
+    checkWhoWonTheGame();
+
+    console.log('PLAYER: ' + game.playerPicked,'HOUSE: ' + game.housePicked);
+};
+
+function checkWhoWonTheGame() {
+    if (game.choices[game.playerPicked].canBeat[game.housePicked] === true) {
+        console.log('You won the game');
+    } else if (game.playerPicked === game.housePicked) {
+        console.log('Draw');
+    } else {
+        console.log('The house won the game');
+    };
 };
